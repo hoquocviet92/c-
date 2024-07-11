03c1d47d164c4d06641ea7636ca7979de4c3bf62
hoquocviet92 <hoquocviet45667@gmail.com>
2024-07-11 22:29:26 +0700

    bai1c+

---
+++ bai1.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main (){
+    float a = 1;
+    float b = 1;
+    float c = 0;
+    float n ;
+    scanf ("%f",&n);
+    for(float i =0;i <= 2; i++){
+        c ++;
+        float h = (a + b)** c ;
+    
+        printf("%f\n",h);
+
+    }
+    return 0;
+
+}
